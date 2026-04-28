54248c8419b749d1ce008815123c4ad98a9f717f
nafisasraboni <nafisasraboni2207@gmail.com>
2026-04-29 03:29:30 +0600

    syntax learn

---
+++ 03_var_let_const/index.js
@@ -0,0 +1,18 @@
+console.log("JavaScript tutorial 3: var, let and const")
+// var a = 45;
+// var a = "p"
+let b = "Harry";
+const author = "Harry"
+// author = 5 // Throws an error because constant cannot be changed
+b = 4
+
+const harry = 0;
+//const harry; // error dibe..const variable_name diye '=' diye kichu na kichu ditey hbe 
+
+let c = null
+let d  = undefined  
+{
+  let b = 'this'
+  console.log(b)//var use hole block manto na..duibar this print korto
+}
+console.log(b)// ekhaneo change kore dito var...let use hoise tay harry print hbe
