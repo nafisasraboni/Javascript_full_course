344666a2fd65f3a3eebb7e4850afed391f84419a
nafisasraboni <nafisasraboni2207@gmail.com>
2026-04-29 03:09:42 +0600

    Learn variables

---
+++ 02_Variables/index.js
@@ -0,0 +1,9 @@
+console.log("this file contains code for javascript course")
+let a = 67 // let varibale declare er jonno use hoi
+console.log(a) 
+a = "Sraboni"
+console.log(a) // 1 ta variable er type k runtime e change korche...dynamically typed language
+
+// rules of variable name:
+// let 8sraboni = 7 //surute number is not allowed as a varibale name;error asbe
+//let var = 7 //not allowed
